7842f9e909d276b8b10da7bb3ae1df1a5698dd60
mabus <m.buscaroli@outlook.com>
2021-09-30 08:50:05 +0100

    Initial Commit

---
+++ iShift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iShift
+//
+//  Created by Matteo on 30/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iShift/iShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  iShiftApp.swift
+//  iShift
+//
+//  Created by Matteo on 30/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct iShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
